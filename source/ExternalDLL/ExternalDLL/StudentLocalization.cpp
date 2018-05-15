982159663ccca821efd602a5433ed39668d18b82
Matthijs <matthijsmud@gmail.com>
2018-05-15 19:00:01 +0200

    Added some more documentation.

--- source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
+++ source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
@@ -23,9 +23,12 @@ bool StudentLocalization::stepFindChinContours(const IntensityImage &image, Feat
 	// Therefore use an offset based on which mouthcorner is the furthest away.
 	// This however could still prove troublesome, so add a slight margin; 
 	// represented by the 'magic' number. 
-	const double radius = chin.getY() - mouthCenter.getY(); //+ std::fmax(\
-		mouthRight.getX() - mouthCenter.getX(),\
-		mouthCenter.getX() - mouthLeft.getX());
+	const double radius = chin.getY() - mouthCenter.getY();
+
+
+	const double smallestOffsetRadiusFactor = 0.75;
+	const double biggestOffsetRadiusFactor = 2;
+	//
 
 	// 180° matches half a circle.
 	// 0° points to the east in goniometrics, where increasing angles are counter-clockwise.
@@ -36,31 +39,36 @@ bool StudentLocalization::stepFindChinContours(const IntensityImage &image, Feat
 	const int angleStepSize = 10;
 	// roughly 19 points need to be found.
 	// 0 through 18 does just that.
-	for (int i = 0; i <= halfCircle; i+=angleStepSize)
+	for (int i = 0; i <= halfCircle / angleStepSize; ++i)
 	{
-		int angle = halfCircle + (i * angleStepSize);
+		// Determine the angle of the line among which we try to look for a point on the chin's contour.
+		int angle = halfCircle + i * angleStepSize;
 		
+		// The size of a step in both the horizontal and verticical direction along the line with the current angle.
 		double xDir = std::cos(angle * M_PI / 180.0);
 		double yDir = -std::sin(angle * M_PI / 180.0);
-		mouthCenter.getX();
-		mouthCenter.getY();
+		
+		// Normalized size of a step that is made alongside the current angle.
 		const double increment = 1;
 
-		for (double distance = radius * 0.7; distance < radius * 2; distance += increment)
+		// Make multiple steps alongside the current angle within bounds where it is likely a contour for the chin.
+		for (double distance = radius * smallestOffsetRadiusFactor; distance < radius * biggestOffsetRadiusFactor; distance += increment)
 		{
+			// Determine the location to check for a contour of the chin.
 			double xCheck = mouthCenter.getX() + (xDir * distance);
 			double yCheck = mouthCenter.getY() + (yDir * distance);
-			const unsigned char treshhold = 32;
+			// Image should be thresholded grayscale, where the pixels are eiher 0 or 255; check in the middle of it.
+			const unsigned char treshhold = 128;
 			if (image.getPixel(xCheck, yCheck) <= treshhold)
 			{
 				chinContour.addPoint(Point2D<double>(xCheck, yCheck));
-				std::cout << "Found contour at: " << xCheck << "; " << yCheck << std::endl;
+				std::cout << "Found contour at: " << xCheck << "; " << yCheck << "\n";
 				break;
 			}
 		}
 	}
 
-	return chinContour.getPoints().size();
+	return (chinContour.getPoints().size() > 0);
 }
 
 bool StudentLocalization::stepFindNoseEndsAndEyes(const IntensityImage &image, FeatureMap &features) const {
